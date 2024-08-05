2b4929e17706d1d8dd51e801ec6be677e300f5b2
Sxm19J <155818064+Sxm19J@users.noreply.github.com>
2024-08-05 20:18:59 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -60,7 +60,7 @@ videos.forEach(video => {
     const videoItem = document.createElement('div');
     videoItem.classList.add('video-item');
     videoItem.innerHTML = `
-        <video controls loop>
+        <video controls loop preload="auto">
             <source src="${video.src}" type="video/mp4">
             Your browser does not support the video tag.
         </video>
